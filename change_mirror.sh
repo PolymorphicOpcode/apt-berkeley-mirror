6c6a592110813a9c73306f17071e6facd62d0058
sangchu <>
2022-06-21 19:29:35 -0600

    updated script

--- change_mirror.sh
+++ change_mirror.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sed -i 's/deb/#deb/g' /etc/apt/sources.list
-sed -i 's/deb-src/#deb-src/g' /etc/apt/sources.list
-echo "deb https://mirrors.ocf.berkeley.edu/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
-echo "deb-src https://mirrors.ocf.berkeley.edu/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+sudo sed -i 's/deb/#deb/g' /etc/apt/sources.list
+sudo sed -i 's/deb-src/#deb-src/g' /etc/apt/sources.list
+sudo echo "deb https://mirrors.ocf.berkeley.edu/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+sudo echo "deb-src https://mirrors.ocf.berkeley.edu/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
 echo "[*] Your sources.list file has been updated!"
+sudo apt update
